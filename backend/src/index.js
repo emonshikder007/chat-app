ec68df174dd40112a0fe62d479b103e5fdead49b
Emon Shikder <emonshikder2217@gmail.com>
2026-06-12 18:03:25 +0600

    problem soving

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,10 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+      origin: [
+    "http://localhost:5173",
+    "https://chatmee.onrender.com/"
+  ],
     credentials: true,
 })
 );
