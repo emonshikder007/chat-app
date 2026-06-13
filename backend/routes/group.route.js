817d81f11c9560a86e771dab3ff1edefc1104589
Emon Shikder <emonshikder2217@gmail.com>
2026-06-13 19:00:41 +0600

    initial commit

--- backend/routes/group.route.js
+++ backend/routes/group.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { createGroup, addMember, removeMember, deleteGroup, getGroups } from "../controllers/group.controller.js";
+import { sendGroupMessage } from "../controllers/message.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -8,6 +9,7 @@ router.post("/create", protectRoute, createGroup);
 router.post("/add-member", protectRoute, addMember);
 router.post("/kick", protectRoute, removeMember);
 router.delete("/:groupId", protectRoute, deleteGroup);
+router.post("/:groupId/send", protectRoute, sendGroupMessage);
 router.get("/grps", protectRoute, getGroups);
 
 
