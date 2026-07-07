3f966a15d057e5e2ad3c3bf1c9cff8433d6a70a1
Emon Shikder <emonshikder2217@gmail.com>
2026-07-07 13:03:14 +0600

    initial commit

--- backend/lib/socket.js
+++ backend/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://chatmee.onrender.com/"],
+    origin: ["http://localhost:5173"],
     credentials: true,
   },
 });
