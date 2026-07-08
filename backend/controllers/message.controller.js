7d56f420a5b2850b914738b041e348f8ee39e481
Emon Shikder <emonshikder2217@gmail.com>
2026-07-08 22:54:55 +0600

    initial commit

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -9,7 +9,7 @@ export const getUsersForSidebar = async (req, res) => {
     const loggedInUserId = req.user._id;
     const filteredUsers = await User.find({
       _id: { $ne: loggedInUserId },
-    }).select("-password fullName email profilePic lastSeen");
+    }).select("-password");
 
     res.status(200).json(filteredUsers);
   } catch (error) {
