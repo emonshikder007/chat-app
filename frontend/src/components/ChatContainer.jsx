447c2a53c96b636a8261727df4d59eeabeefd68a
Emon Shikder <emonshikder2217@gmail.com>
2026-07-10 11:12:59 +0600

    initial commit

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { useChatStore } from "../store/useChatStore";
 import MessageInput from "./MessageInput";
 import ChatHeader from "./ChatHeader";
